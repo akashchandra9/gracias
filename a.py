87ef845774084f21d3c0f2da199309782fd0e08f
prakash24062 <akash85213@gmail.com>
2026-06-20 02:46:22 +0530

    i m in dev

--- a.py
+++ a.py
@@ -3,3 +3,8 @@ print("hello world testing")
 obj = {"a": 1, "b": 2, "c": 3}
 for i,j in obj.items():
     print(i,j)
+
+dict = {"name": "John", "age": 30, "city": "New York"}
+for key, value in dict.items(): 
+    print(key, value)   
+
