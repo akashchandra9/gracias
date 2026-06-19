c59518ec5e09f98d515dbff681afd8001a90f814
prakash24062 <akash85213@gmail.com>
2026-06-20 03:05:17 +0530

    2ND

--- a.py
+++ a.py
@@ -4,6 +4,11 @@ obj = {"a": 1, "b": 2, "c": 3}
 for i,j in obj.items():
     print(i,j)
 
-dict = {"name": "John", "age": 30, "city": "New York"}
+tuple = (1, 2, 3, 4, 5)
+for i in tuple:
+    print(i)
 
+list = [1, 2, 3, 4, 5]
+for i in list:   
+    print(i)
 
